b46215ef3e2d62ce294c9183777ffdcd2ae6e5d7
kanonomaru <kurono4maru5@gmail.com>
2022-04-12 12:54:03 +0000

    [Finish] 3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
   get '/top' => "homes#top"
   get 'book/new'
   post '/books' => "book#create"
   get '/books' => "book#index"
-  get 'book/edit'
-  get '/books/61778'  => 'book#show'
-  get 'book/:id' => 'book#show'
+  get 'books/:id' => 'book#show', as: 'book'
+  get 'books/:id/edit' => 'book#edit', as: 'edit_book'
+  patch 'books/:id' => 'book#update', as: 'update_book'
+  delete 'books/:id' => 'book#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
